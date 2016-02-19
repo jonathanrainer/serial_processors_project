9daa2858f1ee5370b042b78aa3c62221fdda7032
Jonathan Rainer <jr776@york.ac.uk>
2016-02-19 11:16:03 +0000

    Initial commit to this branch to begin work on the second processor

---
+++ hardware/agito/solution1/script.tcl
@@ -0,0 +1,14 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 2015 Xilinx Inc. All rights reserved.
+############################################################
+open_project agito
+open_solution "solution1"
+set_part {xc6slx45csg324-3}
+create_clock -period 20 -name default
+#source "./agito/solution1/directives.tcl"
+#csim_design
+csynth_design
+#cosim_design
+export_design -format ip_catalog
