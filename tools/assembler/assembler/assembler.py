e6c3f3dda11df102591972bfcee06ed3fae19921
Jonathan Rainer <jr776@york.ac.uk>
2016-02-25 17:17:52 +0000

    Started to write my Assembler in Python which should cut down the amount of time it takes to write tests cases

---
+++ tools/assembler/assembler/assembler.py
@@ -0,0 +1,14 @@
+import sys
+import re
+import argparse
+
+class Assembler:
+
+	def parse(self, raw_string):
+		return ""	
+	
+
+if __name__=="__main__":
+	a = Assembler()
+	a.print1()
+
---
+++ tools/assembler/assembler/tests/unit_tests.py
@@ -0,0 +1,17 @@
+import unittest
+from assembler import Assembler
+
+class AssemblerUnitTests(unittest.TestCase):
+
+	a = None
+
+	def setUp(self):
+		self.a = Assembler();
+
+	def test_parse_load(self):
+		output = self.a.parse("LOAD R1 #FF2")
+		self.assertEqual(output, 0x8040FF2)
+		
+
+if __name__ == '__main__':
+	unittest.main()
