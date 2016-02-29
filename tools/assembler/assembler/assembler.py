507b5f4f30a023da49093ad9b2aa7b91c2f3b67c
Jonathan Rainer <jr776@york.ac.uk>
2016-02-29 22:33:06 +0000

    Added a few extra classes but reworked the classes that inherit which should make it very easy to write the rest of assembler

--- tools/assembler/assembler/assembler.py
+++ tools/assembler/assembler/assembler.py
@@ -10,10 +10,20 @@ class InstructionBase(object):
 	@abc.abstractproperty
 	def name(self):
 		return 
+
+	@abc.abstractproperty
+	def opcode(self):
+		return
 	
 	@abc.abstractmethod
 	def create_instruction(self, instruction_elements):
 		return
+
+	def create_hex_representation(self, element_format_pairs):
+		output = ""
+		for (element, format_string) in element_format_pairs:
+			output += format(int(element, base=16), format_string)
+		return format(int(output, base=2), '#010X') 
 
 class Halt(InstructionBase):
 
@@ -21,8 +31,12 @@ class Halt(InstructionBase):
 	def name(self): 
 		return "HALT"
 	
+	@property
+	def opcode(self):
+		return "0"
+
 	def create_instruction(self, instruction_elements):
-		return "0x00000000"
+		return self.create_hex_representation([("0", "032b")])
 
 class Load(InstructionBase):
 
@@ -30,20 +44,71 @@ class Load(InstructionBase):
 	def name(self):
 		return "LOAD"
 	
+	@property
+	def opcode(self):
+		return "1"
+
 	def create_instruction(self, instruction_elements):	
-		register_number = format(int(instruction_elements[0][-1:], base=10), '09b')
-		address = format(int(instruction_elements[1][1:], base=16), '018b')	
-		return "0x" + hex(int("{0}{1}{2}".format("00001", register_number, address), base=2))[2:].upper() 
+		return self.create_hex_representation(
+			[(self.opcode,"05b"), (instruction_elements[0], "09b"), (instruction_elements[1], "018b")])
 
 
 
+class LoadRegisterOffset(InstructionBase):
+
+	@property
+	def name(self):
+		return "LOADRO"
+
+	@property
+	def opcode(self):
+		return "2"
+	
+	def create_instruction(self, instruction_elements):
+		pairs = [(self.opcode, "05b")]
+		pairs.extend([(x, "09b") for x in instruction_elements])
+		return self.create_hex_representation(pairs)  
+
+class Store(InstructionBase):
+
+	@property
+	def name(self):
+		return "STORE"
+	
+	@property
+	def opcode(self):
+		return "3"
+
+	def create_instruction(self, instruction_elements):	
+		return self.create_hex_representation(
+			[(self.opcode,"05b"), (instruction_elements[0], "018b"), (instruction_elements[1], "09b")])
+
+
+
+class StoreRegisterOffset(InstructionBase):
+
+	@property
+	def name(self):
+		return "STORERO"
+
+	@property
+	def opcode(self):
+		return "4"
+	
+	def create_instruction(self, instruction_elements):
+		pairs = [(self.opcode, "05b")]
+		pairs.extend([(x, "09b") for x in instruction_elements])
+		return self.create_hex_representation(pairs)  
+	
+	
 class Assembler(object):
 
 	name_map = {sc().name: sc() for sc in InstructionBase.__subclasses__()}
 
 	def parse(self, raw_string):
 		instruction_elements = re.split(' ', raw_string)
-		return self.name_map[instruction_elements[0]].create_instruction(instruction_elements[1:])	
+		return self.name_map[instruction_elements[0]].create_instruction(
+				[x[1:] for x in instruction_elements[1:]])	
 
 if __name__=="__main__":
 	a  = Assembler()
--- tools/assembler/assembler/tests/unit_tests.py
+++ tools/assembler/assembler/tests/unit_tests.py
@@ -10,12 +10,23 @@ class AssemblerUnitTests(unittest.TestCase):
 
 	def test_parse_halt(self):
 		output = self.a.parse("HALT")
-		self.assertEqual(output, "0x00000000")
+		self.assertEqual(output, "0X00000000")
 
 	def test_parse_load(self):
 		output = self.a.parse("LOAD R1 #FF2")
-		self.assertEqual(output, "0x8040FF2")
-		
+		self.assertEqual(output, "0X08040FF2")
+	
+	def test_parse_load_register_offset(self):
+		output = self.a.parse("LOADRO R1 R2 #27")
+		self.assertEqual(output, "0X10040427")
+
+	def test_parse_store(self):
+		output = self.a.parse("STORE #FFFF R7")
+		self.assertEqual(output, "0X19FFFE07")
+
+	def test_parse_store_register_offset(self):
+		output = self.a.parse("STORERO R8 #42 R4")
+		self.assertEqual(output, "0X20208404")		
 
 if __name__ == '__main__':
 	unittest.main()
