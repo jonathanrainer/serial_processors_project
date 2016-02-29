3ef0d72085a3364174a2ec42f9b56e63850c9008
Jonathan Rainer <jr776@york.ac.uk>
2016-02-29 19:00:23 +0000

    Added new tests and passing code so committing, need to change tack and start trying to add the other HDL modules into the XPS project

--- tools/assembler/assembler/assembler.py
+++ tools/assembler/assembler/assembler.py
@@ -1,14 +1,49 @@
 import sys
 import re
 import argparse
+import abc
 
-class Assembler:
+
+class InstructionBase(object):
+	__metaclass__ = abc.ABCMeta
+
+	@abc.abstractproperty
+	def name(self):
+		return 
+	
+	@abc.abstractmethod
+	def create_instruction(self, instruction_elements):
+		return
+
+class Halt(InstructionBase):
+
+	@property
+	def name(self): 
+		return "HALT"
+	
+	def create_instruction(self, instruction_elements):
+		return "0x00000000"
+
+class Load(InstructionBase):
+
+	@property
+	def name(self):
+		return "LOAD"
+	
+	def create_instruction(self, instruction_elements):	
+		register_number = format(int(instruction_elements[0][-1:], base=10), '09b')
+		address = format(int(instruction_elements[1][1:], base=16), '018b')	
+		return "0x" + hex(int("{0}{1}{2}".format("00001", register_number, address), base=2))[2:].upper() 
+
+
+
+class Assembler(object):
+
+	name_map = {sc().name: sc() for sc in InstructionBase.__subclasses__()}
 
 	def parse(self, raw_string):
-		return ""	
-	
+		instruction_elements = re.split(' ', raw_string)
+		return self.name_map[instruction_elements[0]].create_instruction(instruction_elements[1:])	
 
 if __name__=="__main__":
-	a = Assembler()
-	a.print1()
-
+	a  = Assembler()
--- tools/assembler/assembler/tests/unit_tests.py
+++ tools/assembler/assembler/tests/unit_tests.py
@@ -6,11 +6,15 @@ class AssemblerUnitTests(unittest.TestCase):
 	a = None
 
 	def setUp(self):
-		self.a = Assembler();
+		self.a = Assembler()
+
+	def test_parse_halt(self):
+		output = self.a.parse("HALT")
+		self.assertEqual(output, "0x00000000")
 
 	def test_parse_load(self):
 		output = self.a.parse("LOAD R1 #FF2")
-		self.assertEqual(output, 0x8040FF2)
+		self.assertEqual(output, "0x8040FF2")
 		
 
 if __name__ == '__main__':
