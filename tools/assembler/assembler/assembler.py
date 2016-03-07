faaa83bd701d94b43e3448caf35a2de58675df0a
Jonathan Rainer <jr776@york.ac.uk>
2016-03-07 21:02:24 +0000

    Finished off the obvious test cases and added code to the assembler to aid in the passing of said test cases

--- tools/assembler/assembler/assembler.py
+++ tools/assembler/assembler/assembler.py
@@ -129,6 +129,169 @@ class StoreRegisterOffset(ThreeOperandInstruction):
 	def opcode(self):
 		return "4"
 
+class AddConstant(ThreeOperandInstruction):
+
+        @property
+        def name(self):
+                return "ADDC"
+
+        @property
+        def opcode(self):
+                return "5"
+
+class AddRegister(ThreeOperandInstruction):
+
+        @property
+        def name(self):
+                return "ADD"
+
+        @property
+        def opcode(self):
+                return "6"
+
+class ArithmeticShiftRight(ThreeOperandInstruction):
+
+        @property
+        def name(self):
+                return "ASHR"
+
+        @property
+        def opcode(self):
+                return "7"
+
+class ShiftRight(ThreeOperandInstruction):
+
+        @property
+        def name(self):
+                return "SHR"
+
+        @property
+        def opcode(self):
+                return "8"
+
+class ArithmeticShiftLeft(ThreeOperandInstruction):
+
+        @property
+        def name(self):
+                return "ASHL"
+
+        @property
+        def opcode(self):
+                return "9"
+
+
+class ShiftLeft(ThreeOperandInstruction):
+
+        @property
+        def name(self):
+                return "SHL"
+
+        @property
+        def opcode(self):
+                return "A"
+
+class Complement(TwoOperandInstruction):
+
+        @property
+        def name(self):
+                return "COMP"
+
+        @property
+        def opcode(self):
+                return "B"
+
+        @property
+        def short_long_flag(self):
+                return True
+
+class BranchLessThanRegisterOffset(ThreeOperandInstruction):
+
+        @property
+        def name(self):
+                return "BLTRO"
+
+        @property
+        def opcode(self):
+                return "C"
+
+class BranchLessThanAddress(ThreeOperandInstruction):
+
+        @property
+        def name(self):
+                return "BLTA"
+
+        @property
+        def opcode(self):
+                return "D"
+
+
+class BranchEqualRegisterOffset(ThreeOperandInstruction):
+
+        @property
+        def name(self):
+                return "BERO"
+
+        @property
+        def opcode(self):
+                return "E"
+
+class BranchEqualAddress(ThreeOperandInstruction):
+
+        @property
+        def name(self):
+                return "BEA"
+
+        @property
+        def opcode(self):
+                return "F"
+
+class BranchGreaterThanRegisterOffset(ThreeOperandInstruction):
+
+        @property
+        def name(self):
+                return "BGTRO"
+
+        @property
+        def opcode(self):
+                return "10"
+
+class BranchGreaterThanAddress(ThreeOperandInstruction):
+
+        @property
+        def name(self):
+                return "BGTA"
+
+        @property
+        def opcode(self):
+                return "11"
+
+class BranchRegisterOffset(TwoOperandInstruction):
+
+        @property
+        def name(self):
+                return "BRO"
+
+        @property
+        def opcode(self):
+                return "12"
+
+        @property
+        def short_long_flag(self):
+                return True
+
+class BranchAddress(TwoOperandInstruction):
+
+        @property
+        def name(self):
+                return "BA"
+
+        @property
+        def opcode(self):
+                return "13"
+
+        def short_long_flag(self):
+                return True
+
 class Assembler(object):
 	
 	unflattened_list = [y.__subclasses__() for y in AgitoInstructionBase.__subclasses__()]
