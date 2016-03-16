f11b3901ce11b49c4eeb28f824aeaa8f0fb88dca
Jonathan Rainer <jonrainer@googlemail.com>
2016-03-16 07:44:38 +0000

    Passed first test of whether the instructions can be synthesised or not, now to move through other test cases to add in more instructions as necessary but the principle is there.

--- tools/gouram/gouram.py
+++ tools/gouram/gouram.py
@@ -1,6 +1,7 @@
 import re
 import abc
 
+
 class KuugaPseudoInstruction(object):
 
     @abc.abstractproperty
@@ -19,8 +20,9 @@ class AddPseudoInstruction(KuugaPseudoInstruction):
         return "ADD"
 
     def expand_instruction(self, instruction, start_location):
-        return [["Z", instruction[1], start_location+1], [instruction[2], "Z", start_location+2],
+        return [[instruction[1], "Z", start_location+1], ["Z", instruction[2], start_location+2],
                 ["Z", "Z", start_location+3]]
+
 
 class Gouram(object):
 
@@ -30,6 +32,7 @@ class Gouram(object):
         with open(file_name, 'r') as description_file:
             file_string = description_file.read()
         program = self.split_data_and_code(file_string)
+        program = self.process_program_object(program)
         return self.create_memory_contents(program)
 
     @staticmethod
@@ -43,24 +46,41 @@ class Gouram(object):
         code_list = [re.split(" ", y) for y in [x for x in re.split("\n", code_string) if len(x) > 0]]
         return Program(data_dict, code_list)
 
-    def create_memory_contents(self, program):
+    def process_program_object(self, program):
         program_counter = 0
         expanded_code = []
         for code_line in program.code:
             expanded_code.extend(self.pseudo_instructions[code_line[0]].expand_instruction(code_line, program_counter))
             program_counter = expanded_code[-1][2]
+        # Add in space for a HALT command
+        program_counter += 1
         # Taking Program Size, Create Memory with enough blanks for Code
-        # Replace the placeholders with numbers in the code
-        # Pass to some processing method
+        program.data = {name: [pair[0]+program_counter, pair[1]] for name, pair in program.data.items()}
+        program.code = expanded_code
+        return program
+
+    def create_memory_contents(self, program):
+        result = [self.codeline_to_hexadecimal(x, program.data) for x in program.code]
+        result.extend(["0x00000001"])
+        data_vals = sorted(program.data.values(), key=lambda datum: datum[0])
+        result.extend([format(x[1], "#010x") for x in data_vals])
+        return "{ \"" + "\", \"".join(result) + "\" }"
+
+    @staticmethod
+    def codeline_to_hexadecimal(codeline, data):
+        first_op = data[codeline[0]][0]
+        second_op = data[codeline[1]][0]
+        third_op = codeline[2]
+        binary_string = "{0}{1}{2}00".format(format(first_op, "010b"), format(second_op, "010b"),
+                                             format(third_op, "010b"))
+        return format(int(binary_string, base=2), "#010x")
 
 
 class Program(object):
 
-    data = None
-    code = None
+    data = {}
+    code = []
 
     def __init__(self, data_dict, code_list):
         self.data = data_dict
         self.code = code_list
-
-
--- tools/gouram/tests/integration_tests.py
+++ tools/gouram/tests/integration_tests.py
@@ -11,8 +11,9 @@ class GouramIntegrationTests(unittest.TestCase):
 
     def test_assert_fail_output_is_as_expected(self):
         file = "test_cases/___assert_fail--00.sam"
-        self.assertEqual("{ 0x0180A004, 0x2809008, 0x280A00C, 0x00000001 0x00000025, 0x00000032,0x000000054F, "
-                         "0x00000000, 0x00000010, 0x00000000}", self.gouram.expand(file))
+        self.assertEqual("{ \"0x0180a004\", \"0x02809008\", \"0x0280a00c\", \"0x00000001\", \"0x00000025\", "
+                         "\"0x00000032\", \"0x0000000c\", \"0x0000054f\", \"0x00000000\", \"0x00000010\", "
+                         "\"0x00000000\" }", self.gouram.expand(file))
 
 if __name__ == '__main__':
     unittest.main()
