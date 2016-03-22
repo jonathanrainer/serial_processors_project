3eb6e7d31f25797434944e1b6f47a98ffa067ea4
Jonathan Rainer <jonrainer@googlemail.com>
2016-03-22 14:56:43 +0000

    Created a seemingly working Gills and have added one test case, used as an exemplar, now need to add the other cases in.

--- tools/test_case_generators/gills/gills.py
+++ tools/test_case_generators/gills/gills.py
@@ -1,6 +1,6 @@
 from tools.test_case_generators.raw_file_reader.raw_file_reader import RawFileReader
 from tools.assembler.assembler import Assembler
-
+import sys
 
 class Gills(object):
 
@@ -13,7 +13,16 @@ class Gills(object):
         print("{ " + ", ".join(memory_contents) + " }")
 
     def create_memory_contents(self, program):
-        return 1
+        data_start = len(program.code)
+        program.data = {name: [pair[0]+data_start, pair[1]] for name, pair in program.data.items()}
+        for counter_1, code_line in enumerate(program.code):
+            for counter_2, item in enumerate(code_line):
+                if item in program.data.keys():
+                    program.code[counter_1][counter_2] = "#" + str(program.data[item][0])
+        final_result = [self.assembler.parse(" ".join(x[:-1])).lower() for x in program.code]
+        final_result.extend([format(x[1], '#010x') for x in program.data.values()])
+        return final_result
 
 if __name__ == "__main__":
     g = Gills()
+    g.parse_file(sys.argv[1])
