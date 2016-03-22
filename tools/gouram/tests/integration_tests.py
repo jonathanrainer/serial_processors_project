135c79a64260845a86b97c88a9c45f4873a3667c
Jonathan Rainer <jonrainer@googlemail.com>
2016-03-22 09:39:37 +0000

    Updated the names of the test case files and started adding the real test cases.

--- tools/gouram/tests/integration_tests.py
+++ tools/gouram/tests/integration_tests.py
@@ -10,7 +10,7 @@ class GouramIntegrationTests(unittest.TestCase):
         self.gouram = Gouram()
 
     def test_assert_fail_output_is_as_expected(self):
-        file = "test_cases/___assert_fail--00.sam"
+        file = "test_cases/___assert_fail--00.kpa"
         self.assertEqual("{ 0x0240b004, 0x02c06008, 0x02c0b00c, 0x00000001, 0x00000025, 0x00000032, 0x0000000c, "
                          "0x0000054f, 0x00000000, 0x00000010, 0x00000000, 0x00000000 }", self.gouram.expand(file))
 
